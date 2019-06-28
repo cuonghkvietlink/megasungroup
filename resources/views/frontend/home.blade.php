1d5a7ce685af01c1dc6df5a4336a43736c416452
Kim Cương <kimcuong@Kims-MacBook-Pro.local>
2019-06-28 15:36:49 +0700

    add slide auto play

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -60,6 +60,8 @@
             loop:true,
             margin:10,
             nav:false,
+            autoplay:true,
+            autoplayTimeout:3000,
             responsive:{
                 0:{
                     items:1
